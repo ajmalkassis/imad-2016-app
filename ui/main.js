4fc95d5e0d32e7434c9c60918d10d763c0e47fb1
ajmalkassis <ajmalkassis@users.noreply.github.com>
2016-10-03 14:28:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,10 +7,10 @@ console.log('Loaded!');
 
 //move the img
 var img=document.getElementById("img");
-// var marginLeft=0;
+var marginLeft=0;
 var moveRight=function()
 {
-    var marginLeft=20;
+    
     marginLeft=marginLeft+1;
     img.style.marginLeft=marginLeft+'px';
     
