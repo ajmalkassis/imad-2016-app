86bcbc3582b881ff43dd6fc56dbf760522b6f20d
ajmalkassis <ajmalkassis@users.noreply.github.com>
2016-10-03 15:39:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -4,8 +4,14 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-
-
+var names=[];
+app.get('/submit-name/:name', function(req,res)
+{
+    //get the name from req
+    var name=req.param.name;
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
 var counter=0;
 app.get('/counter',function(req,res)
 {
