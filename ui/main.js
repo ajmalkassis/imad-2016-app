852bf51c82234ba2e7c472d7c93803b18b872aa8
ajmalkassis <ajmalkassis@users.noreply.github.com>
2016-10-03 14:21:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,8 @@
 console.log('Loaded!');
 
 //change the text of help div
-var element=document.getElementById("help");
-element.innerHTML="this is the changed text";
+// var element=document.getElementById("help");
+// element.innerHTML="this is the changed text";
 
 
 //move the img
