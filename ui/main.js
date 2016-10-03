bd14847e321fb866e63a9e156370bb463928d10d
ajmalkassis <ajmalkassis@users.noreply.github.com>
2016-10-03 14:38:53 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,22 +1,36 @@
 console.log('Loaded!');
 
-//change the text of help div
-// var element=document.getElementById("help");
-// element.innerHTML="this is the changed text";
-
-
-//move the img
-var img=document.getElementById("img");
-var marginLeft=0;
-var moveRight=function()
+var btn=document.getElementById("counter");
+var counter=0;
+btn.onclick=function()
 {
-    
-    marginLeft=marginLeft+1;
-    img.style.marginLeft=marginLeft+'px';
-    
-}
+  //counter endpoint
+  
+  //capture the response
+  
+  //render the variable in correct span
+  counter+=1;
+  var sp=document.getElementById("count");
+  sp.innerHTML=sp;
+};
 
-img.onclick=function()
-{
-    var interval=setInterval(moveRight,50);
-}
+// //change the text of help div
+// // var element=document.getElementById("help");
+// // element.innerHTML="this is the changed text";
+
+
+// //move the img
+// var img=document.getElementById("img");
+// var marginLeft=0;
+// var moveRight=function()
+// {
+    
+//     marginLeft=marginLeft+1;
+//     img.style.marginLeft=marginLeft+'px';
+    
+// }
+
+// img.onclick=function()
+// {
+//     var interval=setInterval(moveRight,50);
+// }
